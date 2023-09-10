e1bc7b28440624f6f9911f5e3f38a256a71244ed
Seerat Shaikh <seeratshaikh579@gmail.com>
2023-09-10 22:46:01 +0500

    controllers/middleware/routes/server/models/utils/config

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from "cookie-parser";
 import { notFound, errHandler } from './middleware/errorMiddleware.js';
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000; //look for process.env otherwise go with Port 5000
 import userRoutes from './routes/userRoutes.js';
 
+connectDB();
+
 const app = express();
+
+app.use(express.json()); // Allow us to pas raw json
+app.use(express.urlencoded({ extended: true })) //Allow us to pass form data
+
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
